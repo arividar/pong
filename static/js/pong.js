4a64cdfd4b06a2dbe434efe754ad887ab02a1df2
Ari Johannesson <arividar@gmail.com>
2020-03-17 21:10:02 +0000

    Draw ball

--- static/js/pong.js
+++ static/js/pong.js
@@ -1,10 +1,21 @@
 let canvas = document.getElementById("skjarinn");
 let ctx = canvas.getContext("2d");
 
-const draw = () => {
-    // Do something;
-    requestAnimationFrame(draw);
+const drawBall = (x, y, r) => {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI*2);
+    ctx.fill();
+    ctx.closePath();    
+}
 
+let x = canvas.width/2;
+let y = canvas.height-60;
+let r = 10;
+let rd = 1;
+
+const draw = () => {
+    drawBall(x, y, r);
+    requestAnimationFrame(draw);
 }
 
 requestAnimationFrame(draw);
