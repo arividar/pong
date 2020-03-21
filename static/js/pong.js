a7229587ab04ba1c67f7da5a78abdee9a8ad0750
Ari Johannesson <arividar@gmail.com>
2020-03-21 12:12:03 +0000

    Adds moving bar - needs rework

--- static/js/pong.js
+++ static/js/pong.js
@@ -1,5 +1,20 @@
+'use strict'
+
+const barHeight = 20;
+const barWidth = 140;
+const ballR = 10;
+const barSpeed = 10;
 let canvas = document.getElementById("skjarinn");
 let ctx = canvas.getContext("2d");
+
+let ballX = canvas.width / 2;
+let ballY = canvas.height - 60;
+let dx = 3;
+let dy = 3;
+let barX = canvas.width / 2 - barWidth / 2;
+
+const clearBall = (x, y, r) =>
+  ctx.clearRect(x - r * 2, y - r * 2, x + r * 2, y + r * 2);
 
 const drawBall = (x, y, r) => {
   ctx.beginPath();
@@ -9,30 +24,45 @@ const drawBall = (x, y, r) => {
   ctx.closePath();
 };
 
-const drawBar = (x, width, height) => {
+const clearBar = x => ctx.clearRect(x, canvas.height - barHeight, barWidth, barHeight);
+
+const drawBar = x => {
   ctx.fillStyle = "black";
-  ctx.fillRect(x, canvas.height-height, width, height);
-} 
-
-let x = canvas.width / 2;
-let y = canvas.height - 60;
-let r = 10;
-let dx = 2;
-let dy = 2;
-const barHeight=20;
-const barWidth=140;
-
-const draw = () => {
-  ctx.clearRect(x-r*2, y-r*2, x+r*2, y+r*2);
-  drawBall(x, y, r);
-  drawBar(Math.floor(canvas.width/2-50), barWidth, barHeight);
-  x += dx;
-  y += dy;
-
-  if (x >= canvas.width - r || x <= r) dx = -dx;
-  if (y <= r || y >= canvas.height - r) dy = -dy;
-
-  requestAnimationFrame(draw);
+  ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
 };
 
+const draw = () => {
+
+  clearBall(ballX, ballY, ballR);
+  ballX += dx;
+  ballY += dy;
+  drawBall(ballX, ballY, ballR);
+
+  if (ballX >= canvas.width - ballR || ballX <= ballR) dx = -dx;
+  if (ballY <= ballR || ballY >= canvas.height - ballR) dy = -dy;
+  
+
+  let newBarX = barX;
+  document.onkeydown = function(event) {
+    switch (event.keyCode) {
+       case 37: // left
+          newBarX = barX - barSpeed;
+          break;
+        case 39: // right
+          newBarX = barX + barSpeed;
+          break;
+       case 38: // up
+       case 40: // down
+    }
+    if (newBarX !== barX) {
+      clearBar(barX);
+      barX = newBarX;
+      drawBar(barX);
+    }
+  }
+  drawBar(barX);
+  requestAnimationFrame(draw);
+}
+
 requestAnimationFrame(draw);
+
